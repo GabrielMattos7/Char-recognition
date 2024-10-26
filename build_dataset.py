92a82b437d9cad9ad3c96b4b832c8b43f3b30a45
Lorenzobattistela <lorenzobattistela@gmail.com>
2024-10-26 15:47:59 -0300

    fix build dataset and image with nmew font

--- build_dataset.py
+++ build_dataset.py
@@ -8,22 +8,20 @@ def create_label_files(text_dir, cropped_dir):
         with open(os.path.join(text_dir, text_file), 'r', encoding='utf-8') as f:
             all_text += f.read()
 
-    all_text = re.sub(r'\s+', ' ', all_text)
-    all_text = ''.join(char for char in all_text if char.isprintable() or char.isspace())
+    all_text = re.sub(r'\s+', '', all_text)  # Remove all whitespace
+    all_text = ''.join(char for char in all_text if char.isprintable())  # Remove non-printable characters
 
     cropped_images = sorted([f for f in os.listdir(cropped_dir) if f.endswith('.png')])
 
     # Create label files
-    for i, image_file in enumerate(cropped_images):
-        if i < len(all_text):
-            label = all_text[i]
+    text_index = 0
+    for image_file in cropped_images:
+        if text_index < len(all_text):
+            label = all_text[text_index]
             label_file = os.path.splitext(image_file)[0] + '.label'
             with open(os.path.join(cropped_dir, label_file), 'w', encoding='utf-8') as f:
                 f.write(label)
+            text_index += 1
         else:
             print(f"Warning: More images than characters in text. Skipping {image_file}")
 
-if __name__ == "__main__":
-    text_directory = "./text"
-    cropped_directory = "./cropped_characters"
-    create_label_files(text_directory, cropped_directory)
--- build_images.py
+++ build_images.py
@@ -10,7 +10,7 @@ def create_text_image(text_file, output_file, font_size=40, text_color=(0, 0, 0)
     draw = ImageDraw.Draw(image)
 
     # Use 'arial.ttf' which is typically less bold
-    font = ImageFont.truetype("arial.ttf", font_size)
+    font = ImageFont.truetype("./Inconsolata-Light.ttf", font_size)
 
     max_width = image_size[0] - 20  # 10px padding on each side
     wrapped_text = textwrap.fill(text, width=max_width // (font_size // 2))
@@ -27,13 +27,3 @@ def process_all_text_files(input_dir, output_dir, font_size=20, text_color=(0, 0
             output_path = os.path.join(output_dir, f"{os.path.splitext(filename)[0]}.png")
             create_text_image(input_path, output_path, font_size, text_color, bg_color, image_size)
 
-if __name__ == "__main__":
-    input_directory = "./text"
-    output_directory = "./output_images"
-    
-    font_size = 30
-    text_color = (0, 0, 0)  # Black
-    bg_color = (255, 255, 255)  # White
-    image_size = (1000, 800)
-
-    process_all_text_files(input_directory, output_dir=output_directory, font_size=font_size, text_color=text_color, bg_color=bg_color, image_size=image_size)
